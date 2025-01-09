8d46ef47bd67c9ba18dfb09c7b9686bec376a2f8
vladshein <vlshein1@gmail.com>
2025-01-09 14:53:41 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+  let totalWidth = null;
+  totalWidth =
+    Number.parseFloat(content) +
+    2 * Number.parseFloat(padding) +
+    2 * Number.parseFloat(border);
+  return totalWidth;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
